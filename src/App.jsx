c4cf4875244a55951f1070da48e6ef1aed67667e
4bryssion <acevedomedinasantiago1234@gmail.com>
2026-03-10 17:56:43 -0500

    feat: codigo base del proyecto formativo

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+export default function App(){
+
+  return(
+
+    <div className="min-h-screen bg-green-800 flex items-center justifify-center">
+      <h1 className="text-white text-4xl font-bold">
+        AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+      </h1>
+    </div>
+  );
+}
